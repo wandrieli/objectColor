50bd7fda9f2e7534acc40acbd95693ad898721e6
Wandrieli Nery Barbosa <wandrieli.89@gmail.com>
2020-10-15 21:03:44 -0300

    importacao inicial

---
+++ js/script.js
@@ -0,0 +1,68 @@
+let inputRangeRed = null;
+let inputRangeGreen = null;
+let inputRangeBlue = null;
+
+let inputTextRed = null;
+let inputTextGreen = null;
+let inputTextBlue = null;
+
+let divSquare = null;
+let headColor = null;
+let cicle = null;
+let florDaVida = null;
+
+let r = '0';
+let g = '0';
+let b = '0';
+
+window.addEventListener('load', () => {
+    mapElements()
+    syncDiv()
+})
+
+function mapElements () {
+
+    inputRangeRed = document.querySelector("#inputRangeRed");
+    inputRangeGreen = document.querySelector("#inputRangeGreen");
+    inputRangeBlue = document.querySelector("#inputRangeBlue");
+
+    inputTextRed = document.querySelector("#inputTextRed");
+    inputTextGreen = document.querySelector("#inputTextGreen");
+    inputTextBlue = document.querySelector("#inputTextBlue");
+
+    divSquare = document.querySelector("#divSquare")
+    headColor = document.querySelector('#headColor')
+    cicle = document.querySelector("#cicle")
+    florDaVida = document.querySelector("#florDaVida")
+
+    inputRangeRed.addEventListener('change', handInputRangeChange)
+    inputRangeGreen.addEventListener('change', handInputRangeChange)
+    inputRangeBlue.addEventListener('change', handInputRangeChange)
+}
+
+function handInputRangeChange (event) {
+    const value = event.target.value;
+    const id = event.target.id;
+
+    console.log(id);
+
+    switch (id) {
+        case 'inputRangeRed': r = value; break;
+        case 'inputRangeGreen': g = value; break;
+        case 'inputRangeBlue': b = value; break;
+    }
+
+    syncDiv()
+}
+
+function syncDiv() {
+    //divSquare.style.backgroundColor = `rgb(${r},${g},${b})`;
+    //headColor.style.color = `rgb(${r},${g},${b})`;
+    //cicle.style.backgroundColor = `rgb(${r},${g},${b})`;
+    florDaVida.style.borderColor = `rgb(${r},${g},${b})`;
+    florDaVida.style.backgroundImage = `rgb(${r},${g},${b})`;
+
+    inputTextRed.value = r;
+    inputTextGreen.value = g;
+    inputTextBlue.value = b;
+}
